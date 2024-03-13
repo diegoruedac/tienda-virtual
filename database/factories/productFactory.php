9278cc5577fa5e0c576855ead674982432d5160d
cisco <Nrodriguez895@unab.edu.co>
2024-03-12 20:24:31 -0500

    trabajo en clase 12/3/2024

---
+++ database/factories/productFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class productFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->paragraph(),
+            'price' => $this->faker->randomFloat(2,10000,100000),
+            'url_image' => $this->faker->image(null,640,480),
+            'state' => 1
+        ];
+    }
+}
